d9dfc499b79161d550bbe7477e1ce55c31712c3a
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-05-19 22:39:40 +0200

    Not return error on more than one username

--- code/src/profile/handlers.rs
+++ code/src/profile/handlers.rs
@@ -201,16 +201,13 @@ pub fn get_username(agent_address: Address) -> ZomeApiResult<Option<String>> {
 
     match links_result.links().len() {
         0 => Ok(None),
-        1 => {
+        _ => {
             let username_address = links_result.addresses()[0].clone();
 
             let username: Username = hdk::utils::get_as_type(username_address)?;
 
             Ok(Some(username.username))
         }
-        _ => Err(ZomeApiError::from(String::from(
-            "Agent has more than one username registered",
-        ))),
     }
 }
 
